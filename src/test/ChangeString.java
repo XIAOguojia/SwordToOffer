54f7210ff946bea8bbcfd3267c58ede40c2d1cf3
Raven William <gjnerevmoresf@gmail.com>
2018-07-25 20:56:00 +0800

    添加利用反射修改String类的值

---
+++ src/test/ChangeString.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by intellij IDEA
+ *
+ * @author Raven
+ * Date:2018/7/25
+ * Time:20:45
+ * 通过反射改变String类的值
+ */
+public class ChangeString {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        String s = "abcd";
+        System.out.println("s = " + s);
+        Field valueField = String.class.getDeclaredField("value");
+        valueField.setAccessible(true);
+        char[] value = (char[]) valueField.get(s);
+        value[3] = 'z';
+        System.out.println("s = " + s);
+
+        valueField.set(s,new char[]{'1','2','3'});
+        System.out.println("s = " + s);
+        System.out.println("s.length = " + s.length());
+    }
+}
